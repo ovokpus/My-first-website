c4ae8d20d50497b809a5da0fddeaca725f12494e
ovokpus <ovo@live.ca>
2020-11-16 18:57:31 -0700

    update

--- todo.js
+++ todo.js
@@ -1,16 +1,25 @@
+var todoItems = [];
+
 function sendAlert(message) {
     alert(message);
 }
 
 function addTodoItem() {
     var todo = document.getElementById("todo-item");
-    this.addToList(todo.value);
+    var itemText = todo.value;
+    if (!this.todoItems.includes(itemText)) {
+        this.addToList(itemText);
+    }   
+    
     todo.value = null;
 }
 
 function addToList(message) {
     var list = document.getElementById("todo-list");
     var item = document.createElement("li");
+
+    this.todoItems.push(message);
+    console.log(this.todoItems);
 
     item.appendChild(document.createTextNode(message));
     list.appendChild(item);
