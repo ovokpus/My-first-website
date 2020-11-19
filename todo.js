8b735911fac6108264c887ff97af7bdf356ed6c6
ovokpus <ovo@live.ca>
2020-11-18 18:12:27 -0700

    javascript update

--- todo.js
+++ todo.js
@@ -5,31 +5,28 @@ function addTodoItem() {
     var todo = document.getElementById("todo-item");
     var itemText = todo.value;
 
-    this.removeError();
+    this.removeError(this.getAlert());
 
     if (!this.isExistingItem(itemText)) {
         this.addToList(itemText);
     } else {
-        this.displayError('Item exists')
+        this.displayError('Item exists', this.getAlert())
     }
 
     todo.value = null
-
 }
 
 function getAlert () {
     return document.getElementById("todo-error");
 }
 
-function removeError() {
-    var alert = this.getAlert();
+function removeError(alert) {
     alert.classList.add("invisible");
 
     alert.innerHTML = null;
 }
 
-function displayError(message) {
-    var alert = this.getAlert();
+function displayError(message, alert) {
     alert.classList.remove("invisible");
 
     alert.innerHTML = message;
@@ -41,11 +38,33 @@ function addToList(message) {
 
     this.todoItems.push(message);
 
-
+    item.addEventListener("click", this.completeItem);
     item.appendChild(document.createTextNode(message));
     list.appendChild(item);
 }
-// Need function to add strike to element associated with event
+
+function completeItem(item) {
+    var message = item.target.innerHTML;
+    var strikeout = document.createElement("del");
+
+    strikeout.innerHTML = message;
+
+    item.target.innerHTML = null;
+    item.target.appendChild(strikeout);
+    item.target.removeEventListener("click", completeItem);
+    item.target.addEventListener("click", renewItem);
+}
+
+function renewItem (item) {
+    var message = item.target.innerHTML;
+    var listItem = item.target.parentNode;
+
+    listItem.innerHTML = null;
+    listItem.appendChild(document.createTextNode(message));
+    listItem.removeEventListener("click", renewItem);
+    listItem.addEventListener("click", completeItem);
+}
+
 function isExistingItem(message) {
     return this.todoItems.includes(message);
 }
